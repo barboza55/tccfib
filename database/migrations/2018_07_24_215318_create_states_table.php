02507e4ee2871bb036fe43ab3604c5d6266df1da
ZataraWin <master@limabauru.com.br>
2018-07-24 19:19:32 -0300

    Field Name on table state

--- database/migrations/2018_07_24_215318_create_states_table.php
+++ database/migrations/2018_07_24_215318_create_states_table.php
@@ -15,6 +15,7 @@ class CreateStatesTable extends Migration
     {
         Schema::create('states', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
