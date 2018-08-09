c7d2f1b98b9357815986aa8700cf59dafee634f3
Luidy <ldy80@outlook.com>
2018-08-09 18:33:52 -0300

    minhas mudanças

--- database/migrations/2018_08_07_232230_create_zip_codes_table.php
+++ database/migrations/2018_08_07_232230_create_zip_codes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCepsTable extends Migration
+class CreateZipCodesTable extends Migration
 {
     /**
      * Run the migrations.
