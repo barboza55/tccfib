5ba61223e906c82e38263bb957e691e0e1a1a201
Luidy <ldy80@outlook.com>
2018-08-14 19:09:14 -0300

    documentos do tcc

--- database/migrations/2018_08_08_215215_create_entities_table.php
+++ database/migrations/2018_08_08_215215_create_entities_table.php
@@ -15,6 +15,7 @@ class CreateEntitiesTable extends Migration
     {
         Schema::create('entities', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
