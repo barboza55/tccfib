7a25f5fdd4d5e9a1b8aaa33de9a90067fabc0102
ZataraWin <master@limabauru.com.br>
2018-08-21 12:39:59 -0300

    Inclui a classe UserPassword

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Sian;
 use Illuminate\Support\Facades\Auth;
+use App\UserPassword;
 
 class MediaController extends Controller
 {
