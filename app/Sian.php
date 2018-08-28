d3e528a027a65309af9809fcc57afbc0adf754e0
ZataraWin <master@limabauru.com.br>
2018-08-28 08:37:32 -0300

    soap Finalizado

--- app/Sian.php
+++ app/Sian.php
@@ -93,6 +93,7 @@ class Sian extends Model
         curl_setopt($post, CURLOPT_COOKIEFILE, $this->cookie_file);
 		$error = curl_error($post);
 		curl_exec($post);
+        echo curl_getinfo($post,CURLINFO_TOTAL_TIME);
 		curl_close($post);
     }
 
@@ -170,6 +171,7 @@ class Sian extends Model
 		curl_setopt($post, CURLOPT_COOKIEFILE, $this->cookie_file);
 		//$error = curl_error($post);
 		$html = curl_exec($post);
+        echo curl_getinfo($post, CURLINFO_TOTAL_TIME) . '<br>';
 		$dom = new \DOMDocument();
         @$dom->loadHTML($html);
 		curl_close($post);
