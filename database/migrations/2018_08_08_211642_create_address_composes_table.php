be6913d9e4340248f0abddcd674e9d37c0018f9b
ZataraWin <master@limabauru.com.br>
2018-08-28 21:54:07 -0300

    Resolucao 1084/960 de 2018 protocolado

--- database/migrations/2018_08_08_211642_create_address_composes_table.php
+++ database/migrations/2018_08_08_211642_create_address_composes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateZipCodeStreetsTable extends Migration
+class CreateAddressComposesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,17 @@ class CreateZipCodeStreetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('zip_code_streets', function (Blueprint $table) {
+        Schema::create('address_composes', function (Blueprint $table) {
             $table->increments('id');
+            $table->timestamps();
+
             $table->integer('zip_code_id')->unsigned();
+            $table->integer('district_id')->unsigned();
             $table->integer('street_id')->unsigned();
             $table->integer('data_source_id')->unsigned();
-            $table->timestamps();
+
             $table->foreign('zip_code_id')->references('id')->on('zip_codes')->onUpdate('cascade');
+            $table->foreign('district_id')->references('id')->on('districts')->onUpdate('cascade');
             $table->foreign('street_id')->references('id')->on('streets')->onUpdate('cascade');
             $table->foreign('data_source_id')->references('id')->on('data_sources')->onUpdate('cascade');
         });
@@ -32,6 +36,6 @@ class CreateZipCodeStreetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('zip_code_streets');
+        Schema::dropIfExists('address_composes');
     }
 }
